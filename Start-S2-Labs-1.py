7ce9e5952ffeed0a785bd9f0a7151a4d86fd986b
x <xxx@tsststs.com>
2022-03-24 00:34:18 -0400

    s

---
+++ Start-S2-Labs-1.py
@@ -0,0 +1,16 @@
+# MEE 03/23/22
+
+
+def smash(lst): # Defines function
+    sentence = "" # Creates empty string to add inputs to for final result
+    for i, v in enumerate(lst): # Seperate inputs by there index and value to be used later
+        if i == 0: # the first input of the function will always be added straight to the result list
+            sentence = sentence + v
+        else:
+            sentence = sentence + " " + v # any inputs that come after are added with a space in between
+
+    return sentence # returns final output back to function
+
+# Test Case
+print(smash(["I", "love", "all", "foods"]))
+
